5c896137a8621f3e7d7eeda8a5255f8eb9fa3cb4
SerotoninMicro <nastyantyanpopa@gmail.com>
2023-09-01 21:45:22 +0400

    updt
    span and higlites in main text

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,12 @@ export const About = () => {
             </div>
         </div>
         <div className="About">
-        <p>Junior user interface and user experience designer 
+        <p className="header">Junior user interface and user experience designer 
         with excellent communication and responsiveness skills in the workplace, as well as a proactive attitude 
         and the ability to work effectively even under pressure. Possesses strong skills in various graphic programs, 
-        including Adobe Creative Cloud programs such as Photoshop, Figma, and illustrator, and is adept at programming languages and
-        libraries such as Javascript, CSS, React and HTML as well as the agile and waterfall models. Holds an associate degree in Graphic 
-        Design from the Moscow College of Architecture and Design. Fluent in Russian and B1 level English.</p>
+        including Adobe Creative Cloud programs such as <span1>Photoshop,</span1> <span2>Figma,</span2> and <span3>illustrator,</span3> and is adept at programming languages and
+        libraries such as <span4>Javascript,</span4> <span5>CSS,</span5> <span6>React</span6> and <span7>HTML</span7> as well as the agile and waterfall models. Holds an associate degree in Graphic 
+        Design from the Moscow College of Architecture and Design. Fluent in Russian and <span8>B1 level English.</span8></p>
         </div>
                 </>
     )
