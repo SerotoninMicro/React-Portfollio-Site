c45406025dcd699b3f89e0f7b4953c1fc5bb98ff
SerotoninMicro <nastyantyanpopa@gmail.com>
2023-09-04 21:54:13 +0400

    updt
    nev button

---
+++ About.js
@@ -0,0 +1,28 @@
+export const About = () => {
+    return (
+        <>
+        <div className="Hello">
+        <p>Hello, I'm Anastasia Lebed.</p>
+        </div>
+        <div className="tags">
+            <div className="tag1">
+            <h2>UI/UX Design</h2>
+            </div>
+            <div className="tag2">
+            <h2>Web Developer</h2>
+            </div>
+        </div>
+        <div className="About">
+        <p className="header">Junior user interface and user experience designer 
+        with excellent communication and responsiveness skills in the workplace, as well as a proactive attitude 
+        and the ability to work effectively even under pressure. Possesses strong skills in various graphic programs, 
+        including Adobe Creative Cloud programs such as <span1>Photoshop,</span1> <span2>Figma,</span2> and <span3>illustrator,</span3> and is adept at programming languages and
+        libraries such as <span4>Javascript,</span4> <span5>CSS,</span5> <span6>React</span6> and <span7>HTML</span7> as well as the agile and waterfall models. Holds an associate degree in Graphic 
+        Design from the Moscow College of Architecture and Design. Fluent in Russian and <span8>B1 level English.</span8></p>
+        </div>
+    <button className="BtnCV">
+    <h2>Download CV</h2>
+    </button>
+            </>
+    )
+}
